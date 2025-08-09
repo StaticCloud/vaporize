9e801833153690ef3f40e547469a12ef451a36c3
StaticCloud <diegomarrs@gmail.com>
2025-08-09 15:34:10 -0700

    added header and 404 image handling

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,32 @@
+import requests
+
 class BannerHelper():
     def get_header_urls(self, game_ids):
-        return [f"https://cdn.cloudflare.steamstatic.com/steam/apps/{game_id}/header.jpg" for game_id in game_ids]
-    
+        urls = []
+
+        for id in game_ids:
+            url = f"https://cdn.cloudflare.steamstatic.com/steam/apps/{id}/header.jpg"
+
+            request = requests.get(url)
+
+            if request.status_code == 404:
+                continue;
+
+            urls.append(url)
+        
+        return urls
+
     def get_box_art_urls(self, game_ids):
-        return [f"https://cdn.steamstatic.com/steam/apps/{game_id}/library_600x900.jpg" for game_id in game_ids]
+        urls = []
+
+        for id in game_ids:
+            url = f"https://cdn.steamstatic.com/steam/apps/{id}/library_600x900.jpg"
+
+            request = requests.get(url)
+
+            if request.status_code == 404:
+                continue;
+
+            urls.append(url)
+        
+        return urls
